2bc2d12dabc9d89f7dab684fb5db7112ff69a5ba
Alessandro <alessandro.pirastru.94@gmail.com>
2022-05-07 00:46:04 +0200

    First Commit

---
+++ src/test/java/com/github/excelparser/ExcelParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.excelparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
